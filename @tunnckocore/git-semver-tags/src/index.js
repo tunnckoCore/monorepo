4903e6b55990c97ac891d7cb8feb79ec51771747
Charlike Mike Reagent <mameto2011@gmail.com>
2018-11-06 22:02:49 +0200

    feat: init, temp fork
    Signed-off-by: Charlike Mike Reagent <mameto2011@gmail.com>

--- @tunnckocore/git-semver-tags/src/index.js
+++ @tunnckocore/git-semver-tags/src/index.js
@@ -1,1 +1,55 @@
-export default () => {};
+import proc from 'process';
+import { exec } from '@tunnckocore/execa';
+import semver from 'semver';
+
+const regex = /tag:\s*(.+?)[,)]/gi;
+const cmd = 'git log --decorate --no-color';
+
+function lernaTag(tag, pkg) {
+  if (pkg && !new RegExp(`^${pkg}@`).test(tag)) {
+    return false;
+  }
+
+  // eslint-disable-next-line unicorn/no-unsafe-regex
+  return /^.+@\d+\.\d+\.\d+(-.+)?$/.test(tag);
+}
+
+/* eslint-disable no-param-reassign */
+export default async function gitSemverTags(options) {
+  const opts = Object.assign({ cwd: proc.cwd() }, options);
+
+  if (opts.package && !opts.lernaTags) {
+    throw new Error(
+      'opts.package should only be used when running in lerna mode',
+    );
+  }
+
+  const [{ stdout: data }] = await exec(cmd, opts);
+
+  const tags = [];
+  let tagPrefixRegexp;
+  if (opts.tagPrefix) {
+    tagPrefixRegexp = new RegExp(`^${opts.tagPrefix}(.*)`);
+  }
+  data.split('\n').forEach((decorations) => {
+    let match;
+    /* eslint-disable no-cond-assign */
+    while ((match = regex.exec(decorations))) {
+      const tag = match[1];
+      if (opts.lernaTags) {
+        if (lernaTag(tag, opts.package)) {
+          tags.push(tag);
+        }
+      } else if (opts.tagPrefix) {
+        const matches = tag.match(tagPrefixRegexp);
+        if (matches && semver.valid(matches[1])) {
+          tags.push(tag);
+        }
+      } else if (semver.valid(tag)) {
+        tags.push(tag);
+      }
+    }
+  });
+
+  return tags;
+}
