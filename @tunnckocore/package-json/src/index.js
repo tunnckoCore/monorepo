72d2375a88c5e46544b4888ffec2ee7fd0635c31
Charlike Mike Reagent <mameto2011@gmail.com>
2018-11-06 11:13:57 +0200

    major: update & push to github
    Signed-off-by: Charlike Mike Reagent <mameto2011@gmail.com>

--- @tunnckocore/package-json/src/index.js
+++ @tunnckocore/package-json/src/index.js
@@ -1,1 +1,14 @@
-export default () => {};
+import parse from 'parse-package-name';
+import axios from 'axios';
+
+// packageName: String, endpoint: (name: String, tag: String = 'latest'): String => {}
+export default async function packageJson(packageName, endpoint) {
+  const { name, version } = parse(packageName);
+  const tag = version === '' ? 'latest' : version;
+  const url =
+    typeof endpoint === 'function'
+      ? endpoint(name, tag)
+      : `https://unpkg.com/${name}@${tag}/package.json`;
+
+  return axios.get(url).then((res) => res.data);
+}
--- @tunnckocore/package-json/test/index.js
+++ @tunnckocore/package-json/test/index.js
@@ -1,6 +1,15 @@
 import test from 'asia';
-import mod from '../src';
+import packageJson from '../src';
 
-test('todo', (t) => {
-  t.strictEqual(typeof mod, 'function');
+test('should get package metadata from UNPKG by default', async (t) => {
+  const pkg = await packageJson('@tunnckocore/package-json');
+
+  t.strictEqual(pkg.name, '@tunnckocore/package-json');
+});
+
+test('get package metadata from npm registry', async (t) => {
+  const endpoint = (name, tag) => `https://registry.npmjs.org/${name}/${tag}`;
+  const pkg = await packageJson('package-json@4.0.0', endpoint);
+
+  t.strictEqual(pkg.name, 'package-json');
 });
