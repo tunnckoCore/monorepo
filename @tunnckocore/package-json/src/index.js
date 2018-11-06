358affbbda91c8a5ce50730f51e46c4cfd3f64cc
Charlike Mike Reagent <mameto2011@gmail.com>
2018-11-06 11:26:14 +0200

    fix: run docs
    Signed-off-by: Charlike Mike Reagent <mameto2011@gmail.com>

--- @tunnckocore/package-json/src/index.js
+++ @tunnckocore/package-json/src/index.js
@@ -1,7 +1,28 @@
 import parse from 'parse-package-name';
 import axios from 'axios';
 
-// packageName: String, endpoint: (name: String, tag: String = 'latest'): String => {}
+/**
+ * Get package metadata from the Unpkg instead of NPM registry.
+ * Optionally you can pass `endpoint` function and return the build
+ * the registry url. See the second test in `test/index.js`.
+ *
+ * @example
+ * import packageJson from '@tunnckocore/package-json';
+ *
+ * async function main() {
+ *   console.log(await packageJson('eslint'));
+ *   console.log(await packageJson('package-json@4.0.0'));
+ *   console.log(await packageJson('ava@next'));
+ * }
+ *
+ * main().catch(console.error);
+ *
+ * @name  packageJson
+ * @param {string} packageName the package name, supports `pkg-name@1.2.2` and `pkg-name@next`
+ * @param {function} endpoint like `(name, tag) => url`
+ * @returns {object} package metadata object
+ * @public
+ */
 export default async function packageJson(packageName, endpoint) {
   const { name, version } = parse(packageName);
   const tag = version === '' ? 'latest' : version;
