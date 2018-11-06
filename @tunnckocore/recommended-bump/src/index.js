295105bc706c4bb256817b3a9db2f2cb12fc23c5
Charlike Mike Reagent <mameto2011@gmail.com>
2018-11-06 13:18:11 +0200

    feat: implement and push to github
    Signed-off-by: Charlike Mike Reagent <mameto2011@gmail.com>

--- @tunnckocore/recommended-bump/src/index.js
+++ @tunnckocore/recommended-bump/src/index.js
@@ -1,1 +1,30 @@
-export default () => {};
+import { parse, plugins } from 'parse-commit-message';
+
+/* eslint-disable consistent-return */
+
+export default function recommendedBump(commitMessages) {
+  const commits = []
+    .concat(commitMessages)
+    .filter(Boolean)
+    .map((cmt) => parse(cmt, plugins))
+    .filter((cmt) => /major|minor|patch/.test(cmt.increment));
+
+  if (commits.length === 0) return { increment: false };
+
+  const categorized = commits.reduce((acc, cmt) => {
+    acc[cmt.increment] = acc[cmt.increment] || [];
+    acc[cmt.increment].push(cmt);
+
+    return acc;
+  }, {});
+
+  if (categorized.major) {
+    return { increment: 'major', ...categorized };
+  }
+  if (categorized.minor) {
+    return { increment: 'minor', ...categorized };
+  }
+  if (categorized.patch) {
+    return { increment: 'patch', ...categorized };
+  }
+}
