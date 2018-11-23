4d6219428f8f44cca7cb5a88ded05617a401079d
Charlike Mike Reagent <mameto2011@gmail.com>
2018-11-23 04:04:51 +0200

    feat: run update
    Signed-off-by: Charlike Mike Reagent <mameto2011@gmail.com>

--- @tunnckocore/execa/.prettierrc.js
+++ @tunnckocore/execa/.prettierrc.js
@@ -1,3 +1,3 @@
 'use strict';
 
-module.exports = require('@tunnckocore/config/prettier');
+module.exports = require('eslint-config-tunnckocore/prettier');
--- @tunnckocore/execa/test/index.js
+++ @tunnckocore/execa/test/index.js
@@ -1,19 +1,21 @@
+import assert from 'assert';
 import proc from 'process';
 import test from 'asia';
+
 import { exec, shell } from '../src';
 
-test('export an object with "exec" and "shell" functions', (t) => {
-  t.strictEqual(typeof exec, 'function');
-  t.strictEqual(typeof shell, 'function');
+test('export an object with "exec" and "shell" functions', () => {
+  assert.strictEqual(typeof exec, 'function');
+  assert.strictEqual(typeof shell, 'function');
 });
 
-test('the `exec` accepts arguments with quotes', async (t) => {
+test('the `exec` accepts arguments with quotes', async () => {
   const results = await exec('echo "some content with spaces"');
-  t.strictEqual(results[0].stdout, 'some content with spaces');
+  assert.strictEqual(results[0].stdout, 'some content with spaces');
 });
 
-test('the `shell` should be able to access ENVs', async (t) => {
+test('the `shell` should be able to access ENVs', async () => {
   const results = await shell('echo "foo-$HOME-bar"', { env: proc.env });
 
-  t.ok(/^foo-.*-bar$/.test(results[0].stdout));
+  assert.ok(/^foo-.*-bar$/.test(results[0].stdout));
 });
