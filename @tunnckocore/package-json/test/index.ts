711bb8678be2e33d1234a82bdc258333012f836c
Charlike Mike Reagent <mameto2011@gmail.com>
2019-01-15 17:53:59 +0200

    chore: tweak
    Signed-off-by: Charlike Mike Reagent <mameto2011@gmail.com>

--- @tunnckocore/package-json/test/index.ts
+++ @tunnckocore/package-json/test/index.ts
@@ -7,7 +7,10 @@ test('should get package metadata from UNPKG by default', async () => {
 });
 
 test('get package metadata from npm registry', async () => {
-  const endpoint = (name, tag) => `https://registry.npmjs.org/${name}/${tag}`;
+  function endpoint(name: any, tag: any) {
+    return `https://registry.npmjs.org/${name}/${tag}`;
+  }
+
   const pkg = await packageJson('package-json@4.0.0', endpoint);
 
   expect(pkg.name).toStrictEqual('package-json');
