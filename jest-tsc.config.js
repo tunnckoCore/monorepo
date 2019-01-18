964266097a97c4a8c6063e46b8719849296f2261
Charlike Mike Reagent <mameto2011@gmail.com>
2019-01-19 00:39:54 +0200

    feat: add jest-runner-tsc
    Signed-off-by: Charlike Mike Reagent <mameto2011@gmail.com>

---
+++ jest-tsc.config.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const pkg = require('./package.json');
+
+const EXTENSIONS = pkg.extensions.map((x) => x.slice(1));
+const WORKSPACES = pkg.workspaces.map((x) => x.slice(0, -2));
+
+module.exports = {
+  rootDir: __dirname,
+
+  displayName: 'tsc',
+
+  testEnvironment: 'node',
+  testMatch: ['**/src/**/*'],
+
+  moduleFileExtensions: ['ts', 'tsx'].concat('json'),
+  moduleNameMapper: WORKSPACES.reduce((acc, x) => {
+    acc[`^${x}/(.+)`] = `<rootDir>/${x}/$1/src`;
+    return acc;
+  }, {}),
+
+  runner: 'jest-runner-tsc',
+};
