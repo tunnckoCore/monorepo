f270c9d37c219c32d0bff666f4a94ee3eab9e3b1
Charlike Mike Reagent <mameto2011@gmail.com>
2019-01-23 16:53:09 +0200

    feat(parse-commit-message): cleanup
    Signed-off-by: Charlike Mike Reagent <mameto2011@gmail.com>

--- packages/parse-commit-message/src/commit.ts
+++ packages/parse-commit-message/src/commit.ts
@@ -4,9 +4,6 @@ import { parseHeader, stringifyHeader, validateHeader } from './header';
 
 import { Commit, CommitResult } from './types';
 
-export const FOO_BAR_QUX = 123;
-export const QQBAR = 444;
-
 /**
  * Receives a full commit message `string` and parses it into an `Commit` object
  * and returns it.
