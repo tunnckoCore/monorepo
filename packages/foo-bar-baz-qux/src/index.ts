b71150b22f29471d8af6f9226a1e45cc7e51a4ae
Charlike Mike Reagent <mameto2011@gmail.com>
2019-01-23 15:44:13 +0200

    fix(foo-bar-baz-qux): fake updt hmmmmmm
    Signed-off-by: Charlike Mike Reagent <mameto2011@gmail.com>

--- packages/foo-bar-baz-qux/src/index.ts
+++ packages/foo-bar-baz-qux/src/index.ts
@@ -7,3 +7,7 @@ export function barzzz(a: string, b: string) {
 export function add(foo: number, bar: number) {
   return 10 + foo + bar;
 }
+
+export function doh(foo: string) {
+  return `${foo}-doh`;
+}
