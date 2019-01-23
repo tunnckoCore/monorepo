367aac4d7ab04fc610b5d0692dac72d1af6ab765
Charlike Mike Reagent <mameto2011@gmail.com>
2019-01-23 16:19:22 +0200

    fix(foo-bar-baz-qux): doh doh
    Signed-off-by: Charlike Mike Reagent <mameto2011@gmail.com>

--- packages/foo-bar-baz-qux/src/index.ts
+++ packages/foo-bar-baz-qux/src/index.ts
@@ -7,7 +7,3 @@ export function barzzz(a: string, b: string) {
 export function add(foo: number, bar: number) {
   return 10 + foo + bar;
 }
-
-export function doh(foo: string) {
-  return `${foo}-doh`;
-}
