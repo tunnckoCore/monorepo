0f3ac6d0e102aa363c01a8826e7ff6ec3f6d73f3
Charlike Mike Reagent <mameto2011@gmail.com>
2019-02-11 23:01:37 +0200

    fix(root): cleanup deps
    Signed-off-by: Charlike Mike Reagent <mameto2011@gmail.com>

--- config/eslint.js
+++ config/eslint.js
@@ -1,10 +1,17 @@
 'use strict';
 
+const path = require('path');
 const pkg = require('../package.json');
 const { createAliases } = require('./support');
 
 module.exports = {
-  extends: 'tunnckocore',
+  extends: path.join(
+    path.dirname(__dirname),
+    'packages',
+    'eslint-config-tunnckocore',
+    'src',
+    'index.js',
+  ),
   settings: {
     'import/resolver': {
       'babel-module': createAliases(pkg),
